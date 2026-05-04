d8b1514465e1845c5939d3faf04fa766d720699d
Haixin <haixinhuang502@gmail.com>
2026-05-04 02:21:45 -0700

    feat: create frontend navbar

--- packages/react-frontend/src/components/NavBar.jsx
+++ packages/react-frontend/src/components/NavBar.jsx
@@ -1,2 +1,46 @@
-function NavBar() {}
+import { Menu, X, House, StickyNote, CheckSquare } from "lucide-react";
+
+function NavBar({ activeScreen, onNavigate }) {
+  const getBtnStyle = (screenName) => {
+    const isActive = activeScreen === screenName;
+
+    return {
+      display: "flex",
+      alignItems: "center",
+      gap: "10px",
+      padding: "10px",
+      width: "fit-content",
+      border: "none",
+      background: "transparent",
+      cursor: "pointer",
+      textAlign: "left",
+      borderRadius: "8px",
+      backgroundColor: isActive ? "#dbeafe" : "transparent",
+      color: isActive ? "#1e40af" : "#4b5563",
+    };
+  };
+  return (
+    <div>
+      {/* Menu Button */}
+      <div>
+        <Menu size={24} color="black"></Menu>
+      </div>
+
+      {/* Home Button */}
+      <button style={getBtnStyle("home")} onClick={() => onNavigate("home")}>
+        <House size={24} color="black"></House>
+      </button>
+
+      {/* Notes Button */}
+      <button style={getBtnStyle("notes")} onClick={() => onNavigate("notes")}>
+        <StickyNote size={24} color="black"></StickyNote>
+      </button>
+
+      {/* Todo Button */}
+      <button style={getBtnStyle("todo")} onClick={() => onNavigate("todo")}>
+        <CheckSquare size={24} color="black"></CheckSquare>
+      </button>
+    </div>
+  );
+}
 export default NavBar;
