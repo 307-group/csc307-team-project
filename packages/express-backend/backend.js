f5fcb9e0b76beedc34b626391ab16b689a8f13d1
bobboyyy <147883245+bobboyyy@users.noreply.github.com>
2026-05-14 22:06:09 -0700

    added todo list functionality to backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -16,6 +16,12 @@ const notes = {
   ],
 };
 
+// to-do data store
+const todos = {
+  todos_list: [],
+};
+
+
 app.use(cors());
 app.use(express.json());
 
@@ -59,6 +65,26 @@ app.get("/notes/:id", (req, res) => {
   }
 });
 
+//find to do by id
+const findTodoById = (id) =>
+  todos["todos_list"].find((todo) => todo["id"] === id);
+
+//add to do 
+const addTodo = (todo) => {
+  todos["todos_list"].push(todo);
+  return todo;
+};
+
+//delete to do 
+const deleteTodoById = (id) => {
+  const index = todos["todos_list"].findIndex((todo) => todo["id"] === id);
+  if (index !== -1) {
+    return todos["todos_list"].splice(index, 1);
+  }
+  return undefined;
+};
+
+
 // POST create a note
 app.post("/notes", (req, res) => {
   const noteToAdd = req.body;
@@ -75,6 +101,53 @@ app.delete("/notes/:id", (req, res) => {
     res.status(404).send("Resource not found.");
   } else {
     res.status(200).send(delete_result);
+  }
+});
+
+//GET one todo by ID
+app.get("/todos/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findTodoById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+//GET all todos
+app.get("/todos", (req, res) => {
+  res.send(todos);
+});
+
+//POST create a todo
+app.post("/todos", (req, res) => {
+  const todoToAdd = req.body;
+  todoToAdd["id"] = generateId();
+  let result = addTodo(todoToAdd);
+  res.status(201).send(result);
+});
+
+//DELETE a todo by id
+app.delete("/todos/:id", (req, res) => {
+  const id = req.params["id"];
+  let delete_result = deleteTodoById(id);
+  if (delete_result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.status(200).send(delete_result);
+  }
+});
+
+//PATCH toggle to do complete
+app.patch("/todos/:id", (req, res) => {
+  const id = req.params["id"];
+  let todo = findTodoById(id);
+  if (todo === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    todo["completed"] = !todo["completed"];
+    res.status(200).send(todo);
   }
 });
 
