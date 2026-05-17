c8d85666576ae394fd0af44475cb2bffb408e786
bobboyy <guerrero.erickj@gmail.com>
2026-05-17 11:28:42 -0700

    Revert "installed mongo dependencies and created model files"
    This reverts commit 682561b3e131900c6c19609ce8346666a00e71a6.

--- packages/express-backend/models/note.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  body: { type: String, default: '' },
-});
-
-export default mongoose.model('Note', noteSchema);
