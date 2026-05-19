0d855645e12ed3ed9431a199ef3fda249b3a5896
bobboyy <guerrero.erickj@gmail.com>
2026-05-18 17:43:23 -0700

    fixed naming issue preventing deletion of notes and todos

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -48,7 +48,7 @@ function MyApp() {
     fetch(`${API}/notes/${id}`, { method: 'DELETE' })
       .then((res) => {
         if (res.status === 200) {
-          setNotes(notes.filter((n) => n.id !== id));
+          setNotes(notes.filter((n) => n._id !== id));
         }
       })
       .catch((err) => console.log(err));
@@ -74,7 +74,7 @@ function MyApp() {
       .then((res) => (res.status === 200 ? res.json() : undefined))
       .then((updated) => {
         if (updated) {
-          setTodos(todos.map((t) => (t.id === id ? updated : t)));
+          setTodos(todos.map((t) => (t._id === id ? updated : t)));
         }
       })
       .catch((err) => console.log(err));
@@ -85,7 +85,7 @@ function MyApp() {
     fetch(`${API}/todos/${id}`, { method: 'DELETE' })
       .then((res) => {
         if (res.status === 200) {
-          setTodos(todos.filter((t) => t.id !== id));
+          setTodos(todos.filter((t) => t._id !== id));
         }
       })
       .catch((err) => console.log(err));
