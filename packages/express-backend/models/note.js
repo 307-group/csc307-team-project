c3a7870eb6812f1376a0664a015f7fbf16a1da28
evilmeowie <dashabaitazarova@gmail.com>
2026-05-27 10:55:41 -0700

    fixed some erros

--- packages/express-backend/models/note.js
+++ packages/express-backend/models/note.js
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  body: { type: String, default: "" },
-  labelId: { type: String, default: null },
-});
+const noteSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String, default: "" },
+    labelId: { type: String, default: null },
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model("Note", noteSchema);
