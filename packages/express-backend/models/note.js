516eff1df8d3838ea71917b4e6e503e9659eb634
bobboyyy <147883245+bobboyyy@users.noreply.github.com>
2026-05-27 09:35:14 -0700

    fixed homescreen notes date

--- packages/express-backend/models/note.js
+++ packages/express-backend/models/note.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-const noteSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  body: { type: String, default: "" },
-});
+const noteSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String, default: "" },
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model("Note", noteSchema);
