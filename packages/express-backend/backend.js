40cfd01b91835868fb4b543365cbcdd5ec086ff2
haiixin <106631276+haiixin@users.noreply.github.com>
2026-06-05 10:53:15 -0700

    Remove localhost from allowed origins in CORS

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,7 +34,7 @@ mongoose
 const app = express();
 const port = 8000;
 
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+const allowedOrigins = [process.env.FRONTEND_URL];
 
 app.use(
   cors({
